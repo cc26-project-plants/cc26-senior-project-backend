ffbfdd033bf7e4fb904fc9770907e19468882b10
juliwithoutthee <jnboucree@gmail.com>
2022-05-25 16:28:54 +0900

    Updates plants controller with methods to test HTTP requests

--- src/plants/plants.controller.ts
+++ src/plants/plants.controller.ts
@@ -4,4 +4,18 @@ import { Controller, Get, Post, Put } from "@nestjs/common";
 @Controller('plants')
 export class PlantsController {
 
+    @Get("/:plantId")
+    getPlantById() {
+        return "Plant by Id";
+    }
+
+    @Post()
+    createNewPlant() {
+        return "Create New Plant";
+    }
+
+    @Put('/:plantId')
+    updatePlant() {
+        return "Update plant by id";
+    }
 }
