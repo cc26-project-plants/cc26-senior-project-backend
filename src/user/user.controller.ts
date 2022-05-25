48a2d77195e35b033cbaa68b52b65751bce27fc7
juliwithoutthee <jnboucree@gmail.com>
2022-05-25 11:26:57 +0900

    Creates test HTTP requests and responses for user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Put } from "@nestjs/common";
 
 @Controller('user')
 export class UserController {
     @Get()
-    getUser() {
-        return "Get User"
+    getUsers() {
+        return "Get User Test"; // Don't actually give this data
     }
 
+    @Get('/:userId')
+    getStudentById() {
+        return "Get Student by Id"; // single user
+    }
+
+    @Post()
+    createUser() {
+        return "Create User";
+    }
+
+    @Put('/:userId')
+    updateUser() {
+        return "Updates User by Id";
+    }
+    
 }
