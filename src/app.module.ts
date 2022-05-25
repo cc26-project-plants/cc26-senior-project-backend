3a8af32eae96b78c8956668e4212e20bfae469c2
juliwithoutthee <jnboucree@gmail.com>
2022-05-25 15:41:08 +0900

    Imports plants controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { plantsController } from './plants/plants.controller';
 import { UserController } from "./user/user.controller"
 
 @Module({
   imports: [TypeOrmModule],
-  controllers: [AppController, UserController ],
+  controllers: [AppController, UserController, plantsController ],
   providers: [AppService],
 })
 export class AppModule {}
