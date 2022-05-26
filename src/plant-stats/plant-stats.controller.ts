a716fd2ed83d06856985ac83cbd958a15558a448
juliwithoutthee <jnboucree@gmail.com>
2022-05-26 15:25:45 +0900

    Updates plant stats controller with more methods

--- src/plant-stats/plant-stats.controller.ts
+++ src/plant-stats/plant-stats.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Post, Put, Param, Body } from "@nestjs/common";
+import { Controller, Get, Post, Put, Param, Body, } from "@nestjs/common";
 
 @Controller('plant-stats')
-export class PlantStatsController{
+export class PlantStatsController {
 
+    @Get('/plantId') //double check route 
+    getStatsByPlantId() {
+        return "Stats by Plant Id"; // try to get all stats for that plant
+    }
 
-
+    @Get('/:plantStatsId')
+    getStatbyId() {
+        return "Get stat by id ";
+    }
 
     @Post()
-    createUser( @Body() body) {
+    createStats(@Body() body) {
         console.log(body);
         return "Create Stats"; //should create stats and send data to database
     }
+
+    @Put('/:plantStatsId')
+    updateStats() {
+        return "Updates Stats at id";
+    }
+
 }
