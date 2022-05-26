e2133108e63738253b1e6adb0316bb219922e690
juliwithoutthee <jnboucree@gmail.com>
2022-05-26 12:27:27 +0900

    Creates user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,26 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column() 
+    plants: []; //double check documentation
+
+}
