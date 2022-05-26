9b33ee6bbc79a5de8322e0e8978060d5656fa0f7
juliwithoutthee <jnboucree@gmail.com>
2022-05-26 12:38:20 +0900

    Adds and updates plants entity

---
+++ src/plants/plants.entity.ts
@@ -0,0 +1,18 @@
+import { User } from "src/user/user.entity";
+import { BaseEntity, Column, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export class Plants extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    plantName: string;
+
+    @Column()
+    photo: string;
+
+    @ManyToOne(type => User, user => user.id)
+    user: User; 
+   
+
+};
