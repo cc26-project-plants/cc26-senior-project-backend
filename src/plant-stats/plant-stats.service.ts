22c29e11b682c5f38ae7668eaef399295ee7f51c
Sakiko Yoshida <sacco221@gmail.com>
2022-05-27 18:05:31 +0900

    chore: add imports modules

--- src/plant-stats/plant-stats.service.ts
+++ src/plant-stats/plant-stats.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PlantStats } from './plant-stats.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class PlantStatsService {}
+export class PlantStatsService {
+  constructor(
+    @InjectRepository(PlantStats)
+    private readonly plantstatsRepository: Repository<PlantStats>,
+  ) {}
+}
--- src/plants/plants.controller.ts
+++ src/plants/plants.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Put } from '@nestjs/common';
 import { PlantsService } from './plants.service';
+import { Injectable } from '@nestjs/common';
 
 //Handles plant related HTTP requests
 
