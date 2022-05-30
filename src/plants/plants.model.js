01ddf16e8323f30fc4232c89822b4c5c0c0d338b
Sakiko Yoshida <sacco221@gmail.com>
2022-05-30 17:42:02 +0900

    feat: get post requests for plants and users

--- src/plants/plants.model.js
+++ src/plants/plants.model.js
@@ -1,4 +1,22 @@
+import Plants from "./plants.schema.js";
 
 export default {
-  someFunc() {}
+  async getAllPlants() {
+    try {
+      const allPlants = await Plants.find({});
+      if (!allPlants) return false;
+      return allPlants;
+    } catch (error) {
+      return false;
+    }
+  },
+
+  async createPlant(newData) {
+    try {
+      const newPlants = await Plants.create(newData);
+      return newPlants;
+    } catch (error) {
+      return false;
+    }
+  },
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,5 +4,7 @@ import usersController from "../users/users.controller.js";
 const router = express.Router();
 
 router.get("/", usersController.getAllUsers);
+router.get("/:id", usersController.getByUsername);
+router.post("/", usersController.createUser);
 
 export default router;
